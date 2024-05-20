35d85070f123dfff5b200c2626776ebd08a5b0b8
Khaled Empeshy <empeshy20036969@gmail.com>
2024-05-20 17:35:31 +0300

    1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,18 +2,16 @@
 """ Generate a .tgz achive from the contents of the web_static folder """
 from fabric.api import local
 from datetime import datetime
-import os
+from os.path import isdir
 
 
 def do_pack():
     """ Generate a .tgz achive from the contents of the web_static folder """
-    if not os.path.exists('versions'):
-        os.mkdir('versions')
-
-    curr_time = datetime.now().strftime("%Y%m%d%H%M%S")
-    archive_name = "versions/web_static_{}.tgz".format(curr_time)
-
     try:
+        curr_time = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        archive_name = "versions/web_static_{}.tgz".format(curr_time)
         local("tar -cvzf {} web_static".format(archive_name))
         return archive_name
     except Exception:
