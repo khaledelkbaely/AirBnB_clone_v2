1df19946fed6fce8c049a15925ea3275f3c07cd7
Khaled Empeshy <empeshy20036969@gmail.com>
2024-05-20 17:43:05 +0300

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,23 +8,23 @@ env.hosts = ['54.234.82.159', '100.26.167.203']
 
 def do_deploy(archive_path):
     """ disributes an archive to your web servers """
-    if not exists(archive_path):
+    if exists(archive_path) is False:
         return False
 
     try:
         archive_name = archive_path.split('/')[-1]
         folder = archive_name.split('.')[0]
-        path_for_folder = '/data/web_static/releases'
+        path_for_folder = '/data/web_static/releases/'
 
         put(archive_path, '/tmp/')
-        run('mkdir -p {}/{}/'.format(path_for_folder, folder))
-        run('tar -xzf /tmp/{} -C {}/{}/'.format(
+        run('mkdir -p {}{}/'.format(path_for_folder, folder))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(
             archive_name, path_for_folder, folder))
         run('rm -rf /tmp/{}'.format(archive_name))
-        run('mv {0}/{1}/web_static/* {0}/{1}/'.format(path_for_folder, folder))
-        run('rm -rf {}/{}/web_static'.format(path_for_folder, folder))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path_for_folder, folder))
+        run('rm -rf {}{}/web_static'.format(path_for_folder, folder))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}/{} /data/web_static/current'.format(
+        run('ln -s {}{}/ /data/web_static/current'.format(
             path_for_folder, folder))
         return True
     except Exception:
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
 """ disributes an archive to your web servers """
-from fabric.api import *
-from fabric.operations import put, run, sudo, local
+from fabric.api import env, local, put, run
 from datetime import datetime
-import os
+from os.path import exists, isdir
 
 env.hosts = ['54.234.82.159', '100.26.167.203']
 
 
 def do_pack():
     """ Generate a .tgz achive from the contents of the web_static folder """
-    if not os.path.exists('versions'):
-        os.mkdir('versions')
-
-    curr_time = datetime.now().strftime("%Y%m%d%H%M%S")
-    archive_name = "versions/web_static_{}.tgz".format(curr_time)
-
     try:
+        curr_time = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        archive_name = "versions/web_static_{}.tgz".format(curr_time)
         local("tar -cvzf {} web_static".format(archive_name))
         return archive_name
     except Exception:
@@ -25,24 +22,23 @@ def do_pack():
 
 def do_deploy(archive_path):
     """ disributes an archive to your web servers """
-    if not os.path.isfile(archive_path):
+    if exists(archive_path) is False:
         return False
 
     try:
         archive_name = archive_path.split('/')[-1]
-        path_for_folder = '/data/web_static/releases'
-
-        put(archive_path, '/tmp/{}'.format(archive_name))
         folder = archive_name.split('.')[0]
-        run('mkdir -p {}/{}/'.format(path_for_folder, folder))
-        run('tar -xzf /tmp/{} -C {}/{}/'.format(
+        path_for_folder = '/data/web_static/releases/'
+
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path_for_folder, folder))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(
             archive_name, path_for_folder, folder))
         run('rm -rf /tmp/{}'.format(archive_name))
-        run('mv {}/{}/web_static/* {}/{}/'.format(
-            path_for_folder, folder, path_for_folder, folder))
-        run('rm -rf {}/{}/web_static'.format(path_for_folder, folder))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path_for_folder, folder))
+        run('rm -rf {}{}/web_static'.format(path_for_folder, folder))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}/{} /data/web_static/current'.format(
+        run('ln -s {}{}/ /data/web_static/current'.format(
             path_for_folder, folder))
         return True
     except Exception:
